dde007d5e879fe4d694c6e8d44c5fcc7ed1cc8c5
rickstrahl <rstrahl@west-wind.com>
2020-05-01 14:37:54 -1000

    0.2.2

--- LiveReloadServer/Chocolatey/tools/chocolateyInstall.ps1
+++ LiveReloadServer/Chocolatey/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'LiveReloadWebServer'
-$url = "https://github.com/RickStrahl/Westwind.AspnetCore.LiveReload/raw/9f36b0477a823d20688e2f8ab0b276cdb03dd10d/LiveReloadServer/LiveReloadWebServer.zip"
+$url = "https://github.com/RickStrahl/Westwind.AspnetCore.LiveReload/raw/0.2.2/LiveReloadServer/LiveReloadWebServer.zip"
 $drop = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$sha = "206C66D12BAAD5CEF364D5B958C14E6B7AF8E49835B7C8B7E32A429A948C137B"
+$sha = "D5756444911B2C6CE93BBC9B24BB2E71E57416EC4978D532A0D14B3E208ED106"
 Install-ChocolateyZipPackage -PackageName $packageName -Url $url -UnzipLocation $drop -checksum "$sha" -checksumtype "sha256" 
